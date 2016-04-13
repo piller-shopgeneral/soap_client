5b45ccb09ba04e55a10c4394d0531651811c437a
julianpiller <julianpiller@gmx.de>
2016-04-13 15:38:19 +0200

    push soap client projekt

---
+++ config/magento_soap.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/* 
+ * wsdl url
+ * 
+ * please enter your own wsdl url
+ */
+define('MAGENTO_WSDL_URL', 'http://magento.fittaste.com/api/soap/?wsdl');
+
+/*
+ * soap user name
+ * 
+ * please enter your own user
+ */
+define('MAGENTO_SOAP_USER', 'API_test');
+
+/*
+ * soap password
+ * 
+ * please enter your own password
+ */
+define('MAGENTO_SOAP_PASSWORD', 'letmeinletmein');
+
+?>
